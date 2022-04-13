d28600e962229f76780bab1d207ae50b3bc55522
Kevin Giang <giangkvn@gmail.com>
2022-04-12 22:32:35 -0600

    Create Actions, Types, and setup switch cases in reducer

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,17 +1,36 @@
 import { createStore } from "redux";
 
+export const types = {
+  AMOUNT_CHANGED: 'AMOUNT_CHANGED'
+}
+
 const initialState = {
   amount: 12,
   currencyCode: 'USD'
 }
 
 const reducer = (state = initialState, action) => {
-  return state;
+  switch(action.type) {
+    case types.AMOUNT_CHANGED:
+      return {
+        ...state,
+        amount: action.newAmount
+      }
+    default:
+      return state
+  }
 }
 
 export const selectors = {
   amount: state => state.amount,
   currencyCode: state => state.currencyCode
+}
+
+export const actions = {
+  changeAmount: (newAmount) => ({
+    type: types.AMOUNT_CHANGED,
+    newAmount
+  })
 }
 
 export const store = createStore(reducer)
