86f929c1d5494abb9c0fe45901077f3d7eda1417
Kevin Giang <giangkvn@gmail.com>
2022-04-12 22:32:36 -0600

    Add thunk to redux middleware

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,12 +1,14 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { userReducer } from "./user";
 import { ratesReducer } from "./rates";
+import thunk from "redux-thunk";
 
 export const store = createStore(
   combineReducers({
     user: userReducer,
     rates: ratesReducer,
-  })
+  }),
+  applyMiddleware(thunk)
 );
 
 export default store
