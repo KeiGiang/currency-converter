169dfea256105a0c2823bfef33807bfa3a382ffd
Kevin Giang <giangkvn@gmail.com>
2022-04-12 22:32:36 -0600

    Use store.js as an entry point to the combined reducers

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,46 +1,12 @@
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
+import { userReducer } from "./user";
+import { ratesReducer } from "./rates";
 
-export const types = {
-  AMOUNT_CHANGED: 'AMOUNT_CHANGED',
-  CURRENCY_CHANGED: 'CURRENCY_CHANGED'
-}
-
-const initialState = {
-  amount: 12,
-  currencyCode: 'USD'
-}
-
-const reducer = (state = initialState, action) => {
-  switch(action.type) {
-    case types.AMOUNT_CHANGED:
-      return {
-        ...state,
-        amount: action.newAmount
-      }
-    case types.CURRENCY_CHANGED:
-      return {
-        ...state,
-        currencyCode: action.newCurrency
-      }
-    default:
-      return state
-  }
-}
-
-export const selectors = {
-  amount: state => state.amount,
-  currencyCode: state => state.currencyCode
-}
-
-export const actions = {
-  changeAmount: (newAmount) => ({
-    type: types.AMOUNT_CHANGED,
-    newAmount
-  }),
-  changeCurrency: newCurrency => ({
-    type: types.CURRENCY_CHANGED,
-    newCurrency
+export const store = createStore(
+  combineReducers({
+    user: userReducer,
+    rates: ratesReducer,
   })
-}
+);
 
-export const store = createStore(reducer)
+export default store
